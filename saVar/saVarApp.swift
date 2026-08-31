9ab457b6571381166d4be03d5bac474ef8d1d390
vb10 <vbacik.10@gmail.com>
2026-08-31 15:51:51 +0300

    Initial Commit

---
+++ saVar/saVarApp.swift
@@ -0,0 +1,17 @@
+//
+//  saVarApp.swift
+//  saVar
+//
+//  Created by Veli Bacık on 31.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct saVarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ saVarTests/saVarTests.swift
@@ -0,0 +1,16 @@
+//
+//  saVarTests.swift
+//  saVarTests
+//
+//  Created by Veli Bacık on 31.08.2026.
+//
+
+import Testing
+
+struct saVarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
